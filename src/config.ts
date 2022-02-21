9b7ff61dd79e15253d93c4dcf0c712a2afbfb31f
francisyzy <francisyzy@me.com>
2022-02-22 03:51:53 +0800

    Remove unused configs, update scripts

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  PORT: process.env.PORT || "3000",
-  DATABASE_URL: process.env.DATABASE_URL,
   API_TOKEN: process.env.API_TOKEN,
   LOG_GROUP_ID: process.env.LOG_GROUP_ID,
   ADMIN_TELE_ID: Number(process.env.ADMIN_TELE_ID) || 0,
